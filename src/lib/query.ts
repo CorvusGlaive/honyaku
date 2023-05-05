d4e659e90323bc99cf959bc84435ae66b6726c29
Alex Nas <neos34115@gmail.com>
2023-05-05 18:36:19 +0400

    refactor(query): improve types for fetch

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,21 +1,89 @@
 import { addHistory } from "./history";
 
-class Result {
+type AwaitedReturn<T extends (...args: unknown[]) => Promise<any>> = Awaited<
+	ReturnType<T>
+>;
+type FetchFN = (
+	...args: unknown[]
+) => Promise<{ result: unknown; keys: string[] }>;
+
+export class Query<
+	TQueryFN extends FetchFN,
+	TData = AwaitedReturn<TQueryFN>["result"]
+> {
+	#result: Result<TData>;
+	#key: string;
+	#fn: TQueryFN;
+	#listeners: Set<any>;
+
+	constructor(name: string, fn: TQueryFN) {
+		this.#key = name;
+		this.#fn = fn;
+		this.#result = new Result();
+		this.#listeners = new Set();
+		this.fetch = this.fetch.bind(this);
+		this.setState = this.setState.bind(this);
+	}
+
+	setState(state: Result["state"], dataOrError?: TData) {
+		this.#result.setState(state, dataOrError);
+		const res = Object.create(this);
+		Object.assign(res, this.#result);
+		this.#listeners.forEach((cb) => cb(res));
+	}
+
+	subscribe(fn: (r: Omit<this, "subscribe"> & Result<TData>) => void) {
+		const res = Object.create(this);
+		Object.assign(res, this.#result);
+		delete res.subscribe;
+		fn(res);
+		this.#listeners.add(fn);
+		return () => {
+			this.#listeners.delete(fn);
+		};
+	}
+
+	async fetch(...args: Parameters<TQueryFN>): Promise<AwaitedReturn<TQueryFN>> {
+		this.setState("loading");
+		let res: AwaitedReturn<TQueryFN>;
+
+		try {
+			res = (await this.#fn(...args)) as AwaitedReturn<TQueryFN>;
+			this.setState("success", res.result as TData);
+			this.#addHistory(res);
+		} catch (error) {
+			this.setState("error", error);
+		}
+
+		return res;
+	}
+
+	#addHistory(result: AwaitedReturn<TQueryFN>) {
+		addHistory({
+			service: this.#key,
+			keys: `${this.#key}:${result.keys.join(":")}`,
+			query: result.keys[0],
+			result: result.result,
+		});
+	}
+}
+
+class Result<TData = unknown, TError = Error> {
 	isLoading = false;
 	isError = false;
 	isSuccess = false;
 	state = "initial" as "initial" | "loading" | "error" | "success";
-	data = null;
-	error = null;
+	data: TData = null;
+	error: TError = null;
 
-	setState(st: typeof this.state, dataOrError?: unknown) {
+	setState(st: typeof this.state, dataOrError?: TData | TError) {
 		this.#reset();
 		this.state = st;
 		this.isError = st === "error";
 		this.isLoading = st === "loading";
 		this.isSuccess = st === "success";
-		if (st === "success") this.data = dataOrError;
-		if (st === "error") this.error = dataOrError;
+		if (st === "success") this.data = dataOrError as TData;
+		if (st === "error") this.error = dataOrError as TError;
 		return this;
 	}
 
@@ -26,65 +94,5 @@ class Result {
 		this.data = null;
 		this.error = null;
 		this.state = "initial";
-	}
-}
-
-type FetchFN = (
-	...args: unknown[]
-) => Promise<{ result: unknown; keys: string[] }>;
-export class Query {
-	#result: Result;
-	#key: string;
-	#fn: FetchFN;
-	#listeners: Set<any>;
-
-	constructor(name: string, fn: FetchFN) {
-		this.#key = name;
-		this.#fn = fn;
-		this.#result = new Result();
-		this.#listeners = new Set();
-		this.fetch = this.fetch.bind(this);
-		this.setState = this.setState.bind(this);
-	}
-
-	setState(state: Result["state"], dataOrError?: unknown) {
-		this.#result.setState(state, dataOrError);
-		const res = Object.create(this);
-		Object.assign(res, this.#result);
-		this.#listeners.forEach((cb) => cb(res));
-	}
-
-	subscribe(fn: (r: this & Result) => void) {
-		const res = Object.create(this);
-		Object.assign(res, this.#result);
-		fn(res);
-		this.#listeners.add(fn);
-		return () => {
-			this.#listeners.delete(fn);
-		};
-	}
-
-	async fetch(...args: string[]) {
-		this.setState("loading");
-		let res: Awaited<ReturnType<FetchFN>>;
-
-		try {
-			res = await this.#fn(...args);
-			this.setState("success", res.result);
-			this.#addHistory(res);
-		} catch (error) {
-			this.setState("error", error);
-		}
-
-		return res;
-	}
-
-	#addHistory(result: Awaited<ReturnType<FetchFN>>) {
-		addHistory({
-			service: this.#key,
-			keys: `${this.#key}:${result.keys.join(":")}`,
-			query: result.keys[0],
-			result: result.result,
-		});
 	}
 }
